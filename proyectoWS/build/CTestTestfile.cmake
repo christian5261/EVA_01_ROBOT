9c002ba857e29e022bda86566c6b74d5fe5e0e0c
christian5261 <102389327+christian5261@users.noreply.github.com>
2022-06-24 23:06:01 -0500

    files update

---
+++ proyectoWS/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/ubuntu18/ProyectoFundamentos/proyectoWS/src
+# Build directory: /mnt/c/ubuntu18/ProyectoFundamentos/proyectoWS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("test_pkg")
---
+++ proyectoWS/build/test_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/ubuntu18/ProyectoFundamentos/proyectoWS/src/test_pkg
+# Build directory: /mnt/c/ubuntu18/ProyectoFundamentos/proyectoWS/build/test_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
